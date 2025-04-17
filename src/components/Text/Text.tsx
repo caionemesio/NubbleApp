fbe9244f8d81519ad1630e7eb914354cb4428bff
caionemesio <caionemesio2@gmail.com>
2025-04-17 14:34:30 -0300

    feat: finishing typography config

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -2,16 +2,42 @@ import React from 'react';
 import { Text as RNText, TextProps as RNTextProps, TextStyle } from 'react-native';
 
 interface TextProps extends RNTextProps{
-    preset?:TextVariants
+    preset?:TextVariants,
+    bold?:boolean,
+    italic?:boolean,
+    semiBold?:boolean,
 }
 
 
-export function Text({children,preset = 'paragraphMedium', style, ...rest}: TextProps) {
-    return <RNText style={[$fontSizes[preset],style]} {...rest}>{children}</RNText>;
+export function Text({children,preset = 'paragraphMedium', bold, italic, semiBold, style, ...rest}: TextProps) {
+    const fontFamily = getFontFamily(preset,bold,italic, semiBold);
+    return <RNText style={[$fontSizes[preset],{fontFamily},style]} {...rest}>{children}</RNText>;
 }
 
- type TextVariants ='headingLarge' | 'headingMedium' | 'headingSmall' |
- 'paragraphLarge' | 'paragraphMedium' | 'paragraphSmall' | 'paragraphCaption' | 'paragraphCaptionSmall';
+function getFontFamily(preset:TextVariants, bold?:boolean, italic?:boolean, semiBold?:boolean){
+    if (preset === 'headingLarge' || preset === 'headingMedium' || preset === 'headingSmall'){
+        return italic ? $fontFamily.boldItalic : $fontFamily.bold;
+    }
+    switch(true){
+    case bold && italic:
+        return $fontFamily.boldItalic;
+    case bold:
+        return $fontFamily.bold;
+    case italic:
+        return $fontFamily.italic;
+    case semiBold && italic:
+        return $fontFamily.mediumItalic;
+    case semiBold:
+        return $fontFamily.medium;
+    default:
+        return $fontFamily.regular;
+    }
+
+
+}
+
+type TextVariants ='headingLarge' | 'headingMedium' | 'headingSmall' |
+'paragraphLarge' | 'paragraphMedium' | 'paragraphSmall' | 'paragraphCaption' | 'paragraphCaptionSmall';
 
 const $fontSizes:Record<TextVariants,TextStyle> = {
     headingLarge:{fontSize:32, lineHeight:38.4},
@@ -24,4 +50,17 @@ const $fontSizes:Record<TextVariants,TextStyle> = {
 
     paragraphCaption:{fontSize:12, lineHeight:16.8},
     paragraphCaptionSmall:{fontSize:10, lineHeight:14},
+};
+
+const $fontFamily = {
+    black:'Satoshi-Black',
+    blackItalic:'Satoshi-BlackItalic',
+    bold:'Satoshi-Bold',
+    boldItalic:'Satoshi-BoldItalic',
+    italic:'Satoshi-Italic',
+    light:'Satoshi-Light',
+    lightItalic:'Satoshi-LightItalic',
+    medium:'Satoshi-Medium',
+    mediumItalic:'Satoshi-MediumItalic',
+    regular:'Satoshi-Regular',
 };
