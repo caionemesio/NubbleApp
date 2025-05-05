94ef24d6ac6c07df02f3968039a0554554c7fead
caionemesio <caionemesio2@gmail.com>
2025-05-05 20:44:07 -0300

    feat:config icons

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {Text} from './src/components/Text/Text';
-import {Button} from './src/components/Button/Button';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
+
+import {Icon} from './src/components/Icon/Icon';
+import {Box} from './src/components/Box/Box';
 
 function App(): React.JSX.Element {
   return (
@@ -12,8 +14,22 @@ function App(): React.JSX.Element {
         <Text preset="headingLarge" bold>
           CoffStack
         </Text>
-        <Button title="Primary" disabled/>
-        <Button title="Outline"   preset="primary" />
+        <Box flexDirection="row">
+          <Icon name="chevronRight" size={50} />
+          <Icon name="heartFill" color="buttonPrimary" />
+          <Icon name="profile" size={50} />
+          <Icon name="profileFill" size={50} />
+          <Icon name="heart" size={50} />
+          <Icon name="bellOn" color="carrotSecondary" size={50} />
+        </Box>
+        <Box flexDirection="row">
+          <Icon name="newPost" size={50} />
+          <Icon name="camera" size={50} />
+          <Icon name="chat" size={50} />
+          <Icon name="chatOn" color="error" size={50} />
+          <Icon name="flashOff" size={50} />
+          <Icon name="flashOn" size={50} />
+        </Box>
       </SafeAreaView>
     </ThemeProvider>
   );
